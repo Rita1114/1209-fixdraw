1ee71049dccc9e4ee20226a3afb4ca1bae14662f
LAPTOP-K898G7OD\user <alley8332250@gmail.com>
2022-12-25 21:52:54 +0800

    1225-新增前導動畫(未細修)，修改序章動畫

--- Assets/Scripts/C1/text/C1lMgr.cs
+++ Assets/Scripts/C1/text/C1lMgr.cs
@@ -9,9 +9,12 @@ using Fungus;
 
 public class C1lMgr : MonoBehaviour
 {
-   public static string DebugSceneName;
+    public static string DebugSceneName;
     public static int startPointNumber;//換場景的點
-    public static bool 拿到糖,拿到錢=false;
+    public static bool 拿到糖,拿到錢=false; //C1-L變數部分
+    public static bool 豬公已對話,糖給豬公後=false; //C1-C變數部分
+    public static bool 塗鴉手動畫出現1=false;//C1-P變數部分
+    public static bool 伯已對話, 給錢買筆後=false;//C1-G變數部分
     
     public GameObject playerObject;
 
