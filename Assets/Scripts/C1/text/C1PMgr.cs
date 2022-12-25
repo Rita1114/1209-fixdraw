b38dde6ba6276240b9e8ce76e972dddc55a7742f
LAPTOP-K898G7OD\user <alley8332250@gmail.com>
2022-12-25 22:56:17 +0800

    新增塗鴉手動畫變數(未串聯背包獲得道具等)

--- Assets/Scripts/C1/text/C1PMgr.cs
+++ Assets/Scripts/C1/text/C1PMgr.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fungus;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public class C1PMgr : MonoBehaviour
 {
+    public Flowchart flowchart;
+    public PlayableDirector playableDirector;
+    public static bool 冊被搶=false;
+    public GameObject book;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,11 @@ public class C1PMgr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(flowchart.GetBooleanVariable("冊被搶")==true)
+        {
+            playableDirector.Play();
+            冊被搶 = true; 
+            book.SetActive(false);
+        }
     }
 }
