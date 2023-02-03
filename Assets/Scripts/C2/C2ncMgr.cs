1f9a5a19e46703deda3b139d9aad146b75d72221
LAPTOP-K898G7OD\user <alley8332250@gmail.com>
2023-02-03 15:11:05 +0800

    第一段過場動畫

--- Assets/Scripts/C2/C2ncMgr.cs
+++ Assets/Scripts/C2/C2ncMgr.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fungus;
 using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
 
 public class C2ncMgr : MonoBehaviour
 {
-   
     public Flowchart flowchart;
+    public GameObject image;
+    public PlayableDirector animationController;
+    
     void Start()
     {
        
@@ -16,6 +19,24 @@ public class C2ncMgr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (flowchart.GetBooleanVariable("轉場")==true)
+                {
+                    image.SetActive(true);
+                    if (Input.GetKeyDown(KeyCode.Mouse0))
+                    {
+                        animationController.Play();
+                        GameMgr.中卓走美教動畫已經播過 = true;
+                    }
+                }
+
+        if (GameMgr.中卓走美教動畫已經播過=true)
+        {
+            animationController.enabled = false;
+        }
+    }
+
+    public static void LoadC2()
+    {
+        SceneManager.LoadScene("C2-C");
     }
 }
