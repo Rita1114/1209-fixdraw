c5ae3350f373cf552df12b8aef40c1ad886ba98e
Yixuan <qwezxc77658@gmail.com>
2023-02-06 02:29:56 +0800

    第三章填色遊戲修改完

--- Assets/Scripts/Little Game/C3/ChangeMouse.cs
+++ Assets/Scripts/Little Game/C3/ChangeMouse.cs
@@ -6,19 +6,31 @@ public class ChangeMouse : MonoBehaviour
 {
 
     public Texture2D cursorTexture;
+    public Texture2D oldcursor=null;
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot = Vector2.zero;
 
     public GameObject Game;
+
+    public bool isclick=false;
     void Start()
     {
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        
     }
 
     
     void Update()
     {
-        
+       isclick = Input.GetMouseButton(0);
+        Debug.Log("¨åÄy");
+        if (isclick)
+        {
+            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        }
+        else
+        {
+            Cursor.SetCursor(oldcursor, hotSpot, cursorMode);
+        }
     }
     
 }
