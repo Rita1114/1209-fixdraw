d582c9bec945e628e79f51a9edb843ab5856d4db
LAPTOP-K898G7OD\user <alley8332250@gmail.com>
2023-03-03 20:32:52 +0800

    處理邏輯

--- Assets/Scripts/C2/C2toMgr.cs
+++ Assets/Scripts/C2/C2toMgr.cs
@@ -23,25 +23,24 @@ public class C2toMgr : MonoBehaviour
     {
         if (flowchart.GetBooleanVariable("第一次說")==true)
         {
-            GameMgr.第一次對話 = true;
+            GameMgr.第一次對話完 = true;
         }//對話完畢給管理員知道
-        if (GameMgr.第一次對話==true)
+        if (GameMgr.第一次對話完==true)
         {
-            flowchart.SetBooleanVariable("第二次說",true);
+            flowchart.SetBooleanVariable("第一次說完",true);
         }//透過管理員紀錄設定，避免變數消失
-        
         
         if (flowchart.GetBooleanVariable("第二次說")==true)
         {
-            GameMgr.第二次對話 = true;
+            GameMgr.第二次對話完 = true;
         }//對話完畢給管理員知道
-        if (GameMgr.第二次對話==true)
+        if (GameMgr.第二次對話完==true)
         {
-            flowchart.SetBooleanVariable("第三次說",true);
+            flowchart.SetBooleanVariable("第二次說完",true);
         }//透過管理員紀錄設定，避免變數消失
         
         
-        if (flowchart.GetBooleanVariable("第三次說")==true)
+        if (flowchart.GetBooleanVariable("第二次說完")==true)
         {
             GameMgr.第三次對話 = true;
         }//對話完畢給管理員知道
